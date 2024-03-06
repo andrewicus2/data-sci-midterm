f48beb0513d2838e6e927116e660e5f6944a8667
Drew Brown <andrewrylandbrown@gmail.com>
2024-03-06 14:54:06 +0100

    Added status to scatter chart

--- pages/02 📊 Visualization.py
+++ pages/02 📊 Visualization.py
@@ -39,7 +39,9 @@ lifelowcol.dataframe(average_life_expectancy.tail(5), use_container_width = True
 
 st.header("Correlation Map")
 
-dfHeat = dfviz.drop(['Country', 'Status', 'Year'], axis = 1)
+dfHeat = dfviz.drop(['Country', 'Year'], axis = 1)
+dfHeat["Status"] = dfHeat["Status"].replace({"Developing": 0, "Developed": 1})
+
 
 heatmap = plt.figure(figsize=(20, 12))
 sns.heatmap(dfHeat.corr(), annot=True)
@@ -49,40 +51,40 @@ st.subheader("Individual Factor Impact on Lifespan")
 selected_aspect = st.selectbox(label = "Factor Selector", options = dfviz.columns.drop(["Life Expectancy", "Country", "Year", "Status"]))
 
 if selected_aspect == "Adult Mortality":
-    st.text("Adult Mortality Rates of both sexes (probability of dying between 15 and 60 years per 1000 population)")
+    st.write("Adult Mortality Rates of both sexes (probability of dying between 15 and 60 years per 1000 population)")
 elif selected_aspect == "Infant Deaths":
-    st.text("Number of Infant Deaths per 1000 population")
+    st.write("Number of Infant Deaths per 1000 population")
 elif selected_aspect == "Alcohol Consumption":
-    st.text("Alcohol, recorded per capita (15+) consumption (in litres of pure alcohol)")
+    st.write("Alcohol, recorded per capita (15+) consumption (in litres of pure alcohol)")
 elif selected_aspect == "Percentage Expenditure":
-    st.text("Expenditure on Health as a Percentage of GDP (%)")
+    st.write("Expenditure on Health as a Percentage of GDP (%)")
 elif selected_aspect == "Hepatitis B":
-    st.text("Hepatitis B (HepB) immunization coverage among 1-year-olds (%)")
+    st.write("Hepatitis B (HepB) immunization coverage among 1-year-olds (%)")
 elif selected_aspect == "Measles":
-    st.text("Number of reported cases per 1000 population")
+    st.write("Number of reported cases per 1000 population")
 elif selected_aspect == "BMI":
-    st.text("Average Body Mass Index of entire population")
+    st.write("Average Body Mass Index of entire population")
 elif selected_aspect == "Under-five Deaths":
-    st.text("Number of under-five deaths per 1000 population")
+    st.write("Number of under-five deaths per 1000 population")
 elif selected_aspect == "Polio":
-    st.text("Polio (Pol3) immunization coverage among 1-year-olds (%)")
+    st.write("Polio (Pol3) immunization coverage among 1-year-olds (%)")
 elif selected_aspect == "Total Expenditure":
-    st.text("General government expenditure on health as a percentage of total government expenditure (%)")
+    st.write("General government expenditure on health as a percentage of total government expenditure (%)")
 elif selected_aspect == "Diphtheria":
-    st.text("Diphtheria tetanus toxoid and pertussis (DTP3) immunization coverage among 1-year-olds (%)")
+    st.write("Diphtheria tetanus toxoid and pertussis (DTP3) immunization coverage among 1-year-olds (%)")
 elif selected_aspect == "HIV/AIDS":
-    st.text("Deaths per 1,000 live births HIV/AIDS (0-4 years)")
+    st.write("Deaths per 1,000 live births HIV/AIDS (0-4 years)")
 elif selected_aspect =="GDP":
-    st.text("Gross Domestic Product per capita (in USD)")
+    st.write("Gross Domestic Product per capita (in USD)")
 elif selected_aspect == "Population":
-    st.text("Population of the country")
+    st.write("Population of the country")
 elif selected_aspect =="Thinness 10-19 Years":
-    st.text("Prevalence of thinness among children and adolescents for Age 10 to 19 (%)")
+    st.write("Prevalence of thinness among children and adolescents for Age 10 to 19 (%)")
 elif selected_aspect == "Thinness 5-9 Years":
-    st.text("Prevalence of thinness among children for Age 5 to 9 (%)")
+    st.write("Prevalence of thinness among children for Age 5 to 9 (%)")
 elif selected_aspect == "Income composition of resources":
-    st.text("Human Development Index in terms of income composition of resources (index ranging from 0 to 1)")
+    st.write("Human Development Index in terms of income composition of resources (index ranging from 0 to 1)")
 elif selected_aspect == "Schooling":
-    st.text("Number of years of Schooling (years)")
+    st.write("Number of years of Schooling (years)")
 
-st.scatter_chart(data = dfviz, x = selected_aspect, y = "Life Expectancy")
+st.scatter_chart(data = dfviz, x = selected_aspect, y = "Life Expectancy", color = "Status")
